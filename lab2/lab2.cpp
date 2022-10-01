dabb23565e62c018077ea93f362399db322e8834
User <klyev.alex.02@gmail.com>
2022-10-02 01:32:29 +0700

    Try create search point for Newton method, what is commented out

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -22,9 +22,9 @@ double ddFunA(double x) {
     return 39.6 * x * x + (6 * pow((0.02 + x * x), 2) - 24 * x * x * (0.02 + x * x)) / pow((0.02 + x * x), 4);
 }
 
-double dddFunA(double x) {
+/*double dddFunA(double x) {
     return 79.2 * x - (8 * (6 * pow((0.02 + x * x), 2) - 24 * x * x * (0.02 + x * x)) - 48 * x * x * x - 24 * x * pow((0.02 + x * x), 2)) / pow((0.02 + x * x), 5);
-}
+}*/
 
 double FunB(double x) {
     return  2.7 * pow(sin(x), 3) + 3.3 * pow(cos(x), 3);
@@ -37,11 +37,11 @@ double ddFunB(double x) {
     return -1;
 }
 
-bool Min(double x, double y) {
+bool Max(double x, double y) {
     return x <= y;
 }
 
-bool Max(double x, double y) {
+bool Min(double x, double y) {
     return x >= y;
 }
 
@@ -52,21 +52,21 @@ void Print(std::string name, double(*Fun)(double), double esp, double point, int
     std::cout << "Eps = " << esp << "\nPoint " << operation << " = " << point << "\nFunction value in point " << Fun(point) << "\nCounter = " << counter << std::endl;
 }
 
-double MidPoint(double(*Fun)(double), double a, double b) {
+/*double MidPoint(double(*Fun)(double), double a, double b, bool(*Operator)(double, double)) {
     double x, esp = 0.001;
     double(*dFun)(double) = Fun == FunA ? dFunA : dFunB;
 
     while ((b - a) > 2 * esp) {
         x = (a + b) / 2;
 
-        if (dFun(x) >= 0)
+        if (Operator(dFun(x),0))
             b = x;
         else
             a = x;
     }
 
     return (a + b) / 2;
-}
+}*/
 
 void Indent(int n = 1) {
     for (int i = 0; i < n; i++)
@@ -126,7 +126,14 @@ void Newton(double(*Fun)(double), double esp, double xn) {
     Print("Newton", Fun, esp, x, counter);
 }
 
+/*double SearchPointForNewton(double(*Fun)(double), double a, double b) {
+    double M3, m2, x;
+    M3 = MidPoint(Fun, a, b, Max);
+    m2 = MidPoint(Fun, a, b, Min);
+    x = MidPoint(Fun, a, b, Min);
 
+    return 2*m2/M3 + x + 0.0001;
+}*/
 
 int main()
 {
@@ -140,7 +147,12 @@ int main()
     }
 
     for (double esp = 0.001; esp >= 0.00001; esp /= 100) {
-        Newton(FunA, esp,(a+b)/2);
+        Tangent(FunA, esp, a, b);
+        std::cout << "\n\n";
+    }
+
+    for (double esp = 0.001; esp >= 0.00001; esp /= 100) {
+        Newton(FunA, esp, (a+b)/2);
         std::cout << "\n\n";
     }
 
