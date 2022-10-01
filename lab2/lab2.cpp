a1821eb8c8e031eed8b05a15d6653c728f2e08c5
User <klyev.alex.02@gmail.com>
2022-10-02 01:13:46 +0700

    Added all method for FunA

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -2,14 +2,39 @@
 #include <vector>
 const double PI = 3.141592653589793238463;
 
-double FunA(double x) {
+double F(double x) {
+    return x * x;
+}
 
+double dF(double x) {
+    return 2 * x;
+}
+
+double FunA(double x) {
     return 3.3 * x * x * x * x + 3 / (0.02 + x * x);
 }
 
-double FunB(double x) {
+double dFunA(double x) {
+    return 13.2 * x * x * x - 6 * x / pow((0.02 + x * x), 2);
+}
 
-    return  2.7 * std::pow(std::sin(x), 3) + 3.3 * std::pow(std::cos(x), 3);
+double ddFunA(double x) {
+    return 39.6 * x * x + (6 * pow((0.02 + x * x), 2) - 24 * x * x * (0.02 + x * x)) / pow((0.02 + x * x), 4);
+}
+
+double dddFunA(double x) {
+    return 79.2 * x - (8 * (6 * pow((0.02 + x * x), 2) - 24 * x * x * (0.02 + x * x)) - 48 * x * x * x - 24 * x * pow((0.02 + x * x), 2)) / pow((0.02 + x * x), 5);
+}
+
+double FunB(double x) {
+    return  2.7 * pow(sin(x), 3) + 3.3 * pow(cos(x), 3);
+}
+
+double dFunB(double x) {
+    return -1;
+}
+double ddFunB(double x) {
+    return -1;
 }
 
 bool Min(double x, double y) {
@@ -27,99 +52,99 @@ void Print(std::string name, double(*Fun)(double), double esp, double point, int
     std::cout << "Eps = " << esp << "\nPoint " << operation << " = " << point << "\nFunction value in point " << Fun(point) << "\nCounter = " << counter << std::endl;
 }
 
+double MidPoint(double(*Fun)(double), double a, double b) {
+    double x, esp = 0.001;
+    double(*dFun)(double) = Fun == FunA ? dFunA : dFunB;
+
+    while ((b - a) > 2 * esp) {
+        x = (a + b) / 2;
+
+        if (dFun(x) >= 0)
+            b = x;
+        else
+            a = x;
+    }
+
+    return (a + b) / 2;
+}
+
 void Indent(int n = 1) {
     for (int i = 0; i < n; i++)
         std::cout << "=====================================================================" << std::endl;
     std::cout << "\n\n";
 }
 
-void Dichotomy(double(*Fun)(double), double esp, double a, double b, double delta = 0.001, bool(*Operation)(double, double) = Min) {
-    int counter = 0; double x1, x2;
+void Tangent(double(*Fun)(double), double esp, double a, double b) {
+    int counter = 0; double x1 = 0, x2, dfa, dfb;
+    double(*dFun)(double) = Fun == FunA ? dFunA : dFunB;
+    //dFun = dF; Fun = F;
 
     while ((b - a) > 2 * esp) {
-        x1 = (a + b - delta) / 2;
-        x2 = (a + b + delta) / 2;
+        dfa = dFun(a); dfb = dFun(b);
+        x1 = (Fun(a) - Fun(b) - a * dfa + b * dfb) / (dfb - dfa);
 
-        if (Operation(Fun(x1), Fun(x2)))
-            b = x2;
+        if (dFun(x1) >= 0)
+            b = x1;
         else
             a = x1;
 
         counter++;
     }
 
-    Print("Dichotomy", Fun, esp, (a + b) / 2, counter, Operation);
+    Print("Tangent", Fun, esp, (a + b) / 2, counter);
 }
 
-void GoldenSection(double(*Fun)(double), double esp, double a, double b, bool(*Operation)(double, double) = Min) {
-    int counter = 0; double x1, x2;
+void MidPoint(double(*Fun)(double), double esp, double a, double b) {
+    int counter = 0; double x, dfx;
+    double(*dFun)(double) = Fun == FunA ? dFunA : dFunB;
 
     while ((b - a) > 2 * esp) {
-        x1 = a + (3 - std::sqrt(5)) * (b - a) / 2;
-        x2 = a + (std::sqrt(5) - 1) * (b - a) / 2;
+        x = (a + b) / 2;
+        dfx = dFun(x);
 
-        if (Operation(Fun(x1), Fun(x2)))
-            b = x2;
+        if (dFun(x) >= 0)
+            b = x;
         else
-            a = x1;
+            a = x;
 
         counter++;
     }
 
-    Print("GoldenSection", Fun, esp, (a + b) / 2, counter, Operation);
+    Print("Mid Point", Fun, esp, (a + b) / 2, counter);
 }
+
+void Newton(double(*Fun)(double), double esp, double xn) {
+    int counter = 0; double x = xn;
+    double(*dFun)(double) = Fun == FunA ? dFunA : dFunB;
+    double(*ddFun)(double) = Fun == FunA ? ddFunA : ddFunB;
+    
+    while (abs(dFun(x)) > esp) {
+        x = x - dFun(x) / ddFun(x);
+        counter++;
+    }
+
+    Print("Newton", Fun, esp, x, counter);
+}
+
+
 
 int main()
 {
     std::cout << "Input interval for Fun A" << std::endl;
     double a, b;
     std::cin >> a >> b;
-    double(*Fun)(double) = FunA;
-
 
     for (double esp = 0.001; esp >= 0.00001; esp /= 100) {
-        Dichotomy(Fun, esp, a, b, esp);
+        MidPoint(FunA, esp, a, b);
         std::cout << "\n\n";
     }
 
-    Indent();
-
     for (double esp = 0.001; esp >= 0.00001; esp /= 100) {
-        GoldenSection(Fun, esp, a, b);
+        Newton(FunA, esp,(a+b)/2);
         std::cout << "\n\n";
     }
 
-
-
-    Indent(2);
-    std::cout << "Fun B" << std::endl;
-    std::cout << "Intervals function B" << std::endl;
-
-    std::vector<double> intervals = { 0,0.5,1,3 * PI / 4 };  Fun = FunB;
-
-    for (int i = 0; i < intervals.size() - 1; i++)
-        std::cout << "(" << intervals[i] << ";" << intervals[i + 1] << ") ";
-
-    std::cout << "\n\n";
-
-
-    for (double esp = 0.001; esp >= 0.00001; esp /= 100) {
-        for (int i = 0; i < intervals.size() - 1; i++) {
-            Dichotomy(Fun, esp, intervals[i], intervals[i + 1], esp, i % 2 == 0 ? Max : Min);
-            std::cout << "\n\n";
-        }
-        Indent();
-    }
-
-
-    for (double esp = 0.001; esp >= 0.00001; esp /= 100) {
-        for (int i = 0; i < intervals.size() - 1; i++) {
-            GoldenSection(Fun, esp, intervals[i], intervals[i + 1], i % 2 == 0 ? Max : Min);
-            std::cout << "\n\n";
-        }
-        Indent();
-    }
-
+    
 
 
     return 0;
